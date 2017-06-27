fe3005de09e2a069cc0ce68f410a0aa7f5c2abbf
JP Simard <jp@jpsim.com>
2017-06-27 14:56:08 -0700

    chain bucket notifications to work around only one notification per bucket

--- site-builder/index.js
+++ site-builder/index.js
@@ -3,4 +3,8 @@ var s3 = new AWS.S3();
 
 exports.handler = function(event, context) {
   console.log("site builder event ", JSON.stringify(event));
+  s3.listObjectsV2({Bucket: "protected.pictures4-original"}, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
 };
