152b28e054e5f6bab4e885f160f9d670dd448bba
JP Simard <jp@jpsim.com>
2017-06-27 19:07:08 -0700

    list objects and albums in site builder

--- site-builder/index.js
+++ site-builder/index.js
@@ -1,10 +1,24 @@
 var AWS = require("aws-sdk");
 var s3 = new AWS.S3();
 
+function stripPrefix(object) {
+  return object.Key.replace('pics/original/', '');
+}
+
+function folderName(path) {
+  return path.split('/')[0];
+}
+
 exports.handler = function(event, context) {
-  console.log("site builder event ", JSON.stringify(event));
   s3.listObjectsV2({Bucket: "protected.pictures4-original"}, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
+    if (err) {
+      console.log(err, err.stack);
+      return;
+    }
+    var objects = data.Contents.map(stripPrefix);
+    console.log("objects: " + objects);
+
+    var albums = Array.from(new Set(objects.map(folderName)));
+    console.log("albums: " + albums);
   });
 };
